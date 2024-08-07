916ad9940e2da5e642672b7891e75ddaebe2e724
vinilazzarini <viniciuslazzarini@gmail.com>
2024-08-07 17:09:46 -0300

    Initial commit: Backend

---
+++ src/main/java/com/devini/controledetarefas/ControledetarefasApplication.java
@@ -0,0 +1,13 @@
+package com.devini.controledetarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControledetarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControledetarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devini/controledetarefas/ControledetarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devini.controledetarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledetarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
